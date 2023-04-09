9da886a35c64d977c2f6377774d9126032f1cb5b
Shailendra Gupta <guptas199614@gmail.com>
2023-04-09 13:35:51 +0530

    change in alias

--- alias.sh
+++ alias.sh
@@ -19,9 +19,6 @@ alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
 
-alias md='mkdir -p'
-alias rd=rmdir
-
 function d () {
   if [[ -n $1 ]]; then
     dirs "$@"
@@ -45,73 +42,55 @@ alias mv='mv -i'
 alias v='nvim'
 alias vi='nvim'
 alias vim='nvim'
-# alias dud='du -d 1 -h'
-# alias duf='du -sh *'
+alias dud='du -d 1 -h'
+alias duf='du -sh *'
 # alias fd='find . -type d -name'
 # alias ff='find . -type f -name'
 
-alias cll='colorls -la --report --gs'
 alias cat='bat'
-alias mux='tmuxinator'
+alias tmux='zellij'
 alias nodevm='source "$HOME/.nvm/nvm.sh"'
 alias nohistory='fc -p'
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
-######## GIT
-
-function gitclean() {
-  git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}'
-}
-
-function gitcleanwithdelete() {
-  git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d
-}
-
 # alias gitclean='git branch -r | awk '"'"'{print $1}'"'"' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '"'"'{print $1}'"'"
-
 alias lg='lazygit'
 
 alias ga='git add'
-
 alias gb='git branch'
 alias gba='git branch -a'
 alias gbl='git blame -b -w'
-alias gst='git status --show-stash -b'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gcp='git cherry-pick'
+alias gc='git commit -m'
+alias gca='git commit --amend'
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gdh='git diff HEAD'
-
-alias gco='git checkout'
-alias gcb='git checkout -b'
-alias gcf='git config --list'
-
-alias gignore='git update-index --assume-unchanged'
-
-alias gcs='git commit -S'
-alias gcp='git cherry-pick'
 alias gf='git fetch'
 alias gfa='git fetch --all'
+alias gfp='git fetch --prune'
+alias gfr='git pull --rebase'
 alias gfc='git clone'
 alias gfcr='git clone --recurse-submodules'
 alias gfm='git pull'
-alias gfr='git pull --rebase'
-alias gl='git pull'
 alias gp='git push'
 alias gpf='git push --force-with-lease'
 alias grb='git rebase'
-alias grbi='git rebase -i'
 alias grba='git rebase --abort'
 alias grbc='git rebase --continue'
+alias grbi='git rebase -i'
 alias grbs='git rebase --skip'
-alias gm='git merge'
 alias gs='git stash'
-
 alias glgp='git log --stat -p'
 alias glgl="git log --decorate --oneline --graph --boundary"
 alias glg='git log --date-order --all --graph --format="%C(green)%h%Creset %C(yellow)%an%Creset %C(blue bold)%ar%Creset %C(red bold)%d%Creset%s"'
 alias glg_long="git log --date-order --all --stat --graph --pretty=format:'%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'"
 alias glg_topo='git log --date-order --all --graph --format="%C(green)%h%Creset %C(yellow)%an%Creset %C(blue bold)%ar%Creset %C(red bold)%d%Creset%s"'
 alias glg_long_top0="git log --date-order --all --stat --graph --pretty=format:'%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'"
+
+alias gignore='git update-index --assume-unchanged'
 
 # git log --date=iso --pretty=format:'%ad %H %s' | awk '$0 >= "2018-05-10" && $0 <= "2018-12-31"'
